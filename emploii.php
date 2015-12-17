fb37a044823036ad663c2e40bd1094f17189afce
roukaya <roukayaghanmi@hotmail.com>
2015-12-17 13:40:35 +0100

    emploi de temps

---
+++ emploii.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/stylji.css" />
+        <title>Mon garderie</title>
+    </head>
+    <body >
+	
+	<BODY  height=2000   >
+	<br>
+	<center>
+	<table width=100%>
+	<tr><td width=20%><div align="center"><img src="images/cubee.gif" width=180 height=200></div></td>
+	<td width=80%><MARQUEE BGCOLOR='FFFFFF' DIRECTION='right' WIDTH='100%' HEIGHT='100' VALIGN='bottom' SCROLLAMOUNT='20' SCROLLDELAY='30'><FONT size=8 face= "timesnew roman"color = "#840000" >  <STRONG> Trésors du quartier</STRONG></MARQUEE></td><tr>
+	</table>
+	</center>
+	<center>
+	<ul id="menu">
+      <li><a href="acceuil.php" title="acceuil">acceuil </a> </li>
+        <li><a href="Photo.php" title="Galerie">Galerie</a> </li>
+        <li><a href="emploii.php" title="Horaire"> Horaire</a> </li>
+        <li><a href=" contacts.php" title="Contact"> Contact</a> </li>
+       <li> <a href="plan d'etude.php" title="activités"> activités</a></li>
+	   <li> <a href="inscription.php" title="activités"> Inscription</a></li>
+	    <li> <a href="BD2.php" title="activités"> Payement</a></li>
+		 <li> <a href="index2.php" title="activités">Emloyee</a></li>
+    </ul>
+	</center>
+<BODY  height=2000 >
+	 <center>
+<table  border=1 width=90%>
+ <br><br><br> <br><br><br>
+<marquee DIRECTION='right'> <FONT size=10 color="#660000" face="Harrington" ><STRONG> Horaires à partir du 1 er Septembre 2016</STRONG> </FONT></marquee>
+</caption>
+ <br><br><br><br><br> 
+   <tr>
+     <td rowspan=8>
+   <marquee id="id1"direction="up" scrollamount="2" 
+   height="300" width="450"><span onmouseover="getElementById('id1').stop();" 
+   onmouseout="getElementById('id1').start();">
+   <center>  <img src="images/slider-3.jpg"  />
+    <img src="images/r1.jpg" alt="Photo de montagne" />
+     <img src="images/r2.jpg" alt="Photo de montagne" /> 
+	    <img src="images/r3.jpg" alt="Photo de montagne"/></center>
+     
+	</marquee>
+   </td>
+       <th><font color="#FF0000">Horaires</font></th>
+       <th  ><center>Lundi</th>
+       <th><center>Mardi</th>
+	   <th ><center>Mercredi</th>
+       <th  ><center>Jeudi</th>
+       <th  ><center>Vendredi</th>
+   </tr>
+  
+   <tr > 
+   
+    
+       <td><center>8h30-9h</td>
+       <td ><center>Temps d'accueil</td>
+       <td><center>Temps d'accueil</td>
+	   <td bgcolor="#C0C0C0"> </td>
+	   <td><center>Temps d'accueil</td>
+	   <td><center>Temps d'accueil</td>
+	  
+   </tr>
+   <tr bgcolor="">
+  
+       <td><center>9h-9h30 </center></td>
+       <td ><center>Quoi de neuf<br>(expression orale)</td>
+       <td ><center>Quoi de neuf<br>(expression orale)</td>
+	   <td bgcolor="#C0C0C0" >  </td>
+	   <td ><center>Quoi de neuf<br>(expression orale)</td>
+	   <td ><center>Conseil coopératif</td>
+   </tr>
+    <tr bgcolor="">
+	 
+       <td><center>9h30-9h45</td>
+       <td ><center>Pause (fruits)</td>
+       <td ><center>Pause (fruits)</td>
+	   <td  bgcolor="#C0C0C0" >  </td>
+	   <td ><center>Pause (fruits)</td>
+	   <td ><center>Pause (fruits)</td>
+   </tr>
+   <tr>
+   </td>
+       <td><center>9h45-9h55</td>
+       <td ><center> gym</td>
+       <td ><center>Brain gym</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Brain gym</td>
+	   <td ><center>Brain gym</td>
+   </tr>
+   <tr>
+       <td><center>9h55-10h30</td>
+       <td ><center>Activités en ateliers</td>
+       <td ><center>Activités en ateliers</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Activités en ateliers</td>
+	   <td ><center>Activités en ateliers</td>
+   </tr>
+  <tr>
+   </td>
+       <td><center>10h30-10h50</td>
+       <td ><center>Récréation</td>
+       <td ><center>Récréation</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Récréation</td>
+	   <td ><center>Récréation</td>
+   </tr>
+   <tr> 
+  
+       <td><center>10h50-11h20</td>
+       <td ><center>Activités en ateliers</td>
+       <td ><center>Activités en ateliers</td>
+	   <td bgcolor="#C0C0C0" >  </td>
+	   <td ><center>Activités en ateliers</td>
+	   <td ><center>Activités en ateliers</td>
+   </tr>
+   <tr>
+    <td rowspan=8>
+   <marquee id="id1"direction="up" scrollamount="2" 
+   height="300" width="450"><span onmouseover="getElementById('id1').stop();" 
+   onmouseout="getElementById('id1').start();">
+  <center> <img src="images/slider-3.jpg"  />
+    <img src="images/r1.jpg" alt="Photo de montagne" />
+    <img src="images/r3.jpg" alt="Photo de montagne"/>
+  <img src="images/r2.jpg" alt="Photo de montagne" />	</center></marquee></td>
+   
+       <td><center>11h20-11h40</td>
+       <td ><center>Histoires-Chansons</td>
+      <td ><center>Histoires-Chansons</td>
+	   <td bgcolor="#C0C0C0" >  </td>
+	   <td ><center>Histoires-Chansons</td>
+	   <td ><center>Histoires-Chansons</td>
+   </tr>
+   <tr>
+    
+       <td><center>11h40-13h30</td>
+       <td ><center>Pause repas</td>
+       <td ><center>Pause repas</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Pause repas</td>
+	   <td ><center>Pause repas</td>
+   </tr>
+  <tr>
+       <td><center><center>13h30-14h</td>
+       <td ><center>Temps calme</td>
+       <td ><center>Temps calme</td>
+	   <td bgcolor="#C0C0C0" >  </td>
+	   <td ><center>Temps calme</td>
+	   <td ><center>Temps calme</td>
+   </tr>
+
+     <tr>
+       <td><center>14h-14h30</td>
+       <td ><center>Motricité</td>
+        <td><center>Motricité</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	    <td ><center>Motricité</td>
+		 <td><center>Motricité</td>
+   </tr>
+     <tr>
+       <td><center>14h30-15h</td>
+       <td ><center>Anglais</td>
+        <td ><center>Arts plastiques</td>
+	   <td bgcolor="#C0C0C0"> </td>
+	    <td ><center>Anglais</td>
+        <td ><center>Arts plastiques</td>
+   </tr>
+   <tr>
+       <td><center>15h-15h40</td>
+       <td ><center>Récréation</td>
+       <td ><center>Récréation</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Récréation</td>
+	   <td ><center>Récréation</td>
+   </tr>
+   <tr>
+       <td><center>15h40-16h10</td>
+       <td ><center>Activités en ateliers</td>
+       <td ><center>Activités en ateliers</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td >Activités en ateliers</td>
+	   <td ><center>Activités en ateliers</td>
+   </tr>
+   <tr>
+       <td><center>16h10-16h30</td>
+       <td ><center>Histoires-Chansons</td>
+      <td ><center>Histoires-Chansons</td>
+	   <td bgcolor="#C0C0C0">  </td>
+	   <td ><center>Histoires-Chansons</td>
+	   <td ><center>Histoires-Chansons</td>
+   </tr>
+  </center> 
+</table>
+ 
+ 
+</body>
+</html>
